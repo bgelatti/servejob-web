86b91873faa5f3e9b09b39c082e258d08e99f941
Felipe F. Lima <felipe@ferreiradelima.com>
2014-03-12 18:14:08 -0300

    Disable jquery and bootstrap scripts and fix search type

--- presentation/public/js/app/servejob.layout.js
+++ presentation/public/js/app/servejob.layout.js
@@ -1,8 +1,18 @@
-function searchInput(element) {
-    var value = element.value;
-    if (value) {
-        window.location.hash = "#!/jobs/search/" + value;
-    } else if (window.location.hash.indexOf("#!/jobs/search/") === 0){
-        window.location.hash = "#!/";
+var searchInput = (function () {
+    var timer;
+
+    var searchMethod = function (element) {
+        var value = element.value;
+        clearTimeout(timer);
+
+        timer = setTimeout(function() {
+            if (value) {
+                window.location.hash = "#!/jobs/search/" + value;
+            } else if (window.location.hash.indexOf("#!/jobs/search/") === 0) {
+                window.location.hash = "#!/";
+            }
+        }, 400);
     }
-}
+
+    return searchMethod;
+}());
